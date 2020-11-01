95b158213acd5fbbe33a68a329301d052da5d730
Ukela Cazim <cu011@hdm-stuttgart.de>
2020-11-01 16:59:42 +0100

    Created standart GameLoop

---
+++ src/main/java/de/se2projekt/main/Main.java
@@ -0,0 +1,7 @@
+package de.se2projekt.main;
+
+public class Main {
+    public static void main(String[] args) {
+        new Game(3).start();
+    }
+}
--- src/main/java/org/example/Test.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
