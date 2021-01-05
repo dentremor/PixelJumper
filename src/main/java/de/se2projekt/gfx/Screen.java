980184c6cde127d67b6c025d62f35bef959d3e42
Hiller Daniel <dh102@hdm-stuttgart.de>
2021-01-05 21:20:45 +0100

    Due to the previous refactoring of the editor, the requested functionality does not currently exist

--- src/main/java/de/se2projekt/gfx/Screen.java
+++ src/main/java/de/se2projekt/gfx/Screen.java
@@ -1,24 +1,22 @@
 package de.se2projekt.gfx;
 
-import de.se2projekt.controller.GameController;
 import de.se2projekt.level.tiles.Tile;
-import de.se2projekt.util.TileMap;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
 
 import java.util.LinkedList;
 import java.util.List;
 
 public class Screen {
 
-    private List<Tile> renderedTiles = new LinkedList<>();
+    private final List<Tile> renderedTiles = new LinkedList<>();
 
     public int moveCamera() {
         return 0;
     }
 
-    public void render(GraphicsContext gc, Tile tile, int xOffset) {
-        gc.drawImage(new Image(GameController.class.getResource(tile.getImagePath()).toString()),tile.getX(),tile.getY());
+    public void render(final GraphicsContext gc, final Tile tile, final int xOffset) {
+        //TODO Danny *important* unset comment after adding the missing functionality to TileMap
+//        gc.drawImage(new Image(GameController.class.getResource(tile.getImagePath()).toString()),tile.getX(),tile.getY());
     }
 
     /*
